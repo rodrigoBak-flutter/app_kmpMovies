b3f91bc578aeb98d873c5ba5a7f348a38e25cdf1
Rodrigo <rodrigosebastianbak@hotmail.com>
2025-03-08 12:20:45 +0100

    Add mode dark and light

--- composeApp/src/androidMain/kotlin/com/bakapp/movies/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/bakapp/movies/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.bakapp.movies
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
+            val view = LocalView.current
+            var darkMode = isSystemInDarkTheme()
+            if(!view.isInEditMode){
+                val window = (view.context as Activity).window
+                window.statusBarColor = Color.Transparent.toArgb()
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkMode
+            }
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
--- composeApp/src/commonMain/kotlin/com/bakapp/movies/ui/screens/Screen.kt
+++ composeApp/src/commonMain/kotlin/com/bakapp/movies/ui/screens/Screen.kt
@@ -1,14 +1,19 @@
 package com.bakapp.movies.ui.screens
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Screen(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
-    MaterialTheme {
+    val isDarkMode = isSystemInDarkTheme()
+    var colorScheme = if (isDarkMode) darkColorScheme() else lightColorScheme()
+    MaterialTheme(colorScheme) {
         Surface (
             modifier = Modifier.fillMaxSize(),
             content = content
